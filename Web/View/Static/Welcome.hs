630239422a66ce22b7e560ef72bfdcb833d6af37
Thomas Hellstrom <thktomska@gmail.com>
2023-09-06 14:07:00 +0400

    Add posts migration

--- Web/View/Static/Welcome.hs
+++ Web/View/Static/Welcome.hs
@@ -38,5 +38,5 @@ instance View WelcomeView where
               <p style="color: hsla(196, 13%, 50%, 1); margin-top: 4rem">
                  You can modify this start page by making changes to "./Web/View/Static/Welcome.hs".
               </p>
-         </div> 
+         </div>
 |]
